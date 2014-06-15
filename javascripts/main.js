26ce9ebe11e592a4e3e06f26438b1738ebce629a
kalamsrinivas <kalamsrinivas@gmail.com>
2014-06-16 01:48:02 +0530

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -29,3 +29,4 @@ console.log('This would be the main JS file.');
 	});
 
 })(jQuery);
+;(function(e){e(function(){e(".nav").append(e('<div class="nav-mobile"></div>'));e(".nav-item").has("ul").prepend('<span class="nav-click"><i class="nav-arrow"></i></span>');e(".nav-mobile").click(function(){e(".nav-list").toggle()});e(".nav-list").on("click",".nav-click",function(){e(this).siblings(".nav-submenu").toggle();e(this).children(".nav-arrow").toggleClass("nav-rotate")})})})(jQuery);
